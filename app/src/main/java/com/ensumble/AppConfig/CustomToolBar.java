06f17b2a1f71b0d2a1cd656811ea56102b5e8323
Ibrahim <IbrahimSayed94@outlook.com>
2019-07-19 17:00:14 +0200

    search product

--- app/src/main/java/com/ensumble/AppConfig/CustomToolBar.java
+++ app/src/main/java/com/ensumble/AppConfig/CustomToolBar.java
@@ -6,10 +6,14 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ensumble.R;
+import com.ensumble.view.activity.SearchProductActivity;
 
 public class CustomToolBar {
     Toolbar toolbar;
@@ -19,6 +23,7 @@ public class CustomToolBar {
     DrawerLayout drawer_layout;
 
     TextView  txt_toolbarTitle;
+    ImageView toolbar_search;
 
     public CustomToolBar() {
     }
@@ -35,6 +40,7 @@ public class CustomToolBar {
         toolbar = activity.findViewById(R.id.tool_bar);
         drawer_layout = activity.findViewById(R.id.drawer_layout);
         txt_toolbarTitle = activity.findViewById(R.id.txt_toolbarTitle);
+        toolbar_search = activity.findViewById(R.id.toolbar_search);
         ((AppCompatActivity) (activity)).setSupportActionBar(toolbar);
         ActionBar actionBar = ((AppCompatActivity) (activity)).getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(false);
@@ -58,6 +64,16 @@ public class CustomToolBar {
                 });
             }
         }
+
+
+
+        toolbar_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity, SearchProductActivity.class);
+                activity.startActivity(intent);
+            }
+        });
     } // function of initToolBar
 
     public void setTitle(String title)
